0a247a5cbd090adc1c94783dd0737ef47b05fc46
Zihadul <22-47087-1@student.aiub.edu>
2026-08-29 03:25:03 +0600

    Add billing calculator unit tests

---
+++ PharmacyManagement/BillingCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PharmacyManagement
+{
+    public static class BillingCalculator
+    {
+        public static int CalculateLineTotal(int quantity, int unitPrice)
+        {
+            if (quantity <= 0 || unitPrice <= 0)
+            {
+                return 0;
+            }
+
+            checked
+            {
+                return quantity * unitPrice;
+            }
+        }
+
+        public static bool CanFulfil(int requestedQuantity, int availableQuantity)
+        {
+            return requestedQuantity > 0 && availableQuantity >= requestedQuantity;
+        }
+    }
+}
